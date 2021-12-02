2522e01673a56843b22bdb49c2a9f2ecc3c9ec52
Abalaca <95268786+Abalaca@users.noreply.github.com>
2021-12-03 00:02:05 +0800

    Add files via upload

---
+++ assets/js/chartdiv4.js
@@ -0,0 +1,47 @@
+am5.ready(function() {
+
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("chartdiv4");
+    
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+    
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+    var chart = root.container.children.push(
+      am5percent.PieChart.new(root, {
+        endAngle: 270
+      })
+    );
+    
+    // Create series
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+    var series = chart.series.push(
+      am5percent.PieSeries.new(root, {
+        valueField: "value",
+        categoryField: "category",
+        endAngle: 270
+      })
+    );
+    
+    series.states.create("hidden", {
+      endAngle: -90
+    });
+    
+    // Set data
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+    series.data.setAll([{
+      category: "No sexual harassment in the workplace",
+      value: 33.5
+    }, {
+      category: "Sexual harassment in the workplace",
+      value: 66.5
+    }]);
+    
+    series.appear(1000, 100);
+    
+    }); // end am5.ready()
