2028f0e79592b0feb4feb5cd296ed13e9ec09445
Abalaca <95268786+Abalaca@users.noreply.github.com>
2021-12-04 15:36:19 +0800

    Update chartdiv4.js

--- assets/js/chartdiv4.js
+++ assets/js/chartdiv4.js
@@ -1,47 +1,103 @@
-am5.ready(function() {
+am4core.ready(function () {
 
-    // Create root element
-    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-    var root = am5.Root.new("chartdiv4");
+  // Themes begin
+  am4core.useTheme(am4themes_animated);
+  // Themes end
+
+  // Create chart instance
+  var chart = am4core.create("chartdiv4", am4charts.XYChart);
+
+  // Add data
+  chart.data = [
+      {
+          "age": "Put up posters that demean women in the workplace",
+          "vulnerable group": 22
+      },
+      {
+          "age": "Look pornographically without consent",
+          "vulnerable group": 58
+      },
+      {
+          "age": "Talk about sex-related topics without consent in office",
+          "vulnerable group": 92
+      },
+     {
+          "age": "Tease about sex at a party",
+          "vulnerable group": 73
+      },
+
+      {
+        "age": "Continue private invitations after being rejected",
+        "vulnerable group": 29
+    },
     
-    // Set themes
-    // https://www.amcharts.com/docs/v5/concepts/themes/
-    root.setThemes([
-      am5themes_Animated.new(root)
-    ]);
-    
-    // Create chart
-    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
-    var chart = root.container.children.push(
-      am5percent.PieChart.new(root, {
-        endAngle: 270
-      })
-    );
-    
-    // Create series
-    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
-    var series = chart.series.push(
-      am5percent.PieSeries.new(root, {
-        valueField: "value",
-        categoryField: "category",
-        endAngle: 270
-      })
-    );
-    
-    series.states.create("hidden", {
-      endAngle: -90
-    });
-    
-    // Set data
-    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
-    series.data.setAll([{
-      category: "No sexual harassment in the workplace",
-      value: 33.5
-    }, {
-      category: "Sexual harassment in the workplace",
-      value: 66.5
-    }]);
-    
-    series.appear(1000, 100);
-    
-    }); // end am5.ready()
+  {
+    "age": "Act obscenely to others without consent",
+    "vulnerable group": 14
+},
+{
+  "age": "Touch a person's body without consent",
+  "vulnerable group": 61
+},
+
+{
+  "age": "Reveive sexual and unpleasant messages through social media",
+  "vulnerable group": 33
+},
+{
+  "age": "Imply that others will be retaliated against\nif they don’t cooperate in sexual matters",
+  "vulnerable group": 7
+},
+{
+  "age": "Force sex",
+  "vulnerable group": 3
+},
+{
+  "age": "Force people to take photos with exposed bodies",
+  "vulnerable group": 1
+}
+  ];
+
+  // Create axes
+
+  var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.dataFields.category = "age";
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.renderer.minGridDistance = 30;
+
+  // categoryAxis.renderer.labels.template.adapter.add("dy", function (dy, target) {
+  //     if (target.dataItem && target.dataItem.index & 2 == 2) {
+  //         return dy + 25;
+  //     }
+  //     return dy;
+  // });
+
+  var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+
+  // Create series
+  var series = chart.series.push(new am4charts.ColumnSeries());
+  series.dataFields.valueX = "vulnerable group";
+  series.dataFields.categoryY = "age";
+  series.columns.template.tooltipText = "{categoryY}: [bold]{valueX}[/]";
+  series.columns.template.fillOpacity = .8;
+
+  var columnTemplate = series.columns.template;
+  columnTemplate.strokeWidth = 2;
+  columnTemplate.strokeOpacity = 1;
+  var title = chart.titles.create();
+  title.text = "Types of sexual harassment women suffer";
+  title.fontSize = 20;
+  title.marginTop = 20;
+  title.marginBottom = 20;
+
+  series.columns.template.adapter.add("fill", function (fill, target) {
+      return chart.colors.getIndex(target.dataItem.index);
+  });
+  // alert(chart.colors.getIndex(0));
+  // alert(chart.colors.getIndex(1));
+});
+
+
+
+
+// end am4core.ready()
