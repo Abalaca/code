b49f94e0bb4e41cb35f53c861f88ee683c62de75
Abalaca <95268786+Abalaca@users.noreply.github.com>
2021-12-08 15:49:47 +0800

    Update chartdiv6.js

--- assets/js/chartdiv6.js
+++ assets/js/chartdiv6.js
@@ -11,147 +11,139 @@ am4core.ready(function () {
     // Add data
     chart.data = [
         {
-            "Category": 2006,
-            "Overall Rank": 63,
+            "Category": 2006,            
             "Economic Participation": 53,
             "Educational Attainment": 77,
             "Health and Survival": 144,
-            "Political Empowerment": 52
+            "Political Empowerment": 52,
+            "Overall Rank": 63
         },
         {
             "Category": 2007,
-            "Overall Rank": 73,
             "Economic Participation": 60,
             "Educational Attainment": 91,
             "Health and Survival": 124,
-            "Political Empowerment": 59
+            "Political Empowerment": 59,
+            "Overall Rank": 73
         },
         {
             "Category": 2008,
-            "Overall Rank": 57,
             "Economic Participation": 43,
             "Educational Attainment": 87,
             "Health and Survival": 126,
-            "Political Empowerment": 54
+            "Political Empowerment": 54,
+            "Overall Rank": 57
         },
         {
             "Category": 2009,
-            "Overall Rank": 60,
             "Economic Participation": 38,
             "Educational Attainment": 87,
             "Health and Survival": 130,
-            "Political Empowerment": 60
+            "Political Empowerment": 60,
+            "Overall Rank": 60
         },
         {
             "Category": 2010,
-            "Overall Rank": 61,
             "Economic Participation": 46,
             "Educational Attainment":88,
             "Health and Survival": 133,
-            "Political Empowerment": 56
+            "Political Empowerment": 56,
+            "Overall Rank": 61
         },
         {
             "Category": 2011,
-            "Overall Rank": 61,
             "Economic Participation":50,
             "Educational Attainment": 85,
             "Health and Survival": 133,
-            "Political Empowerment": 57
+            "Political Empowerment": 57,
+            "Overall Rank": 61
         },
         {
             "Category": 2012,
-            "Overall Rank":69,
             "Economic Participation": 58,
             "Educational Attainment": 85,
             "Health and Survival": 132,
-            "Political Empowerment": 58
+            "Political Empowerment": 58,
+            "Overall Rank":69
         },
         {
             "Category": 2013,
-            "Overall Rank":69,
             "Economic Participation": 62,
             "Educational Attainment": 81,
             "Health and Survival": 133,
-            "Political Empowerment":59
+            "Political Empowerment":59,
+            "Overall Rank":69
         },
         {
             "Category": 2014,
-            "Overall Rank": 87,
             "Economic Participation":76,
             "Educational Attainment": 89,
             "Health and Survival": 140,
-            "Political Empowerment": 72
+            "Political Empowerment": 72,
+            "Overall Rank": 87
         },
         {
             "Category": 2015,
-            "Overall Rank": 91,
             "Economic Participation": 81,
             "Educational Attainment": 83,
             "Health and Survival":145,
-            "Political Empowerment": 73
+            "Political Empowerment": 73,
+            "Overall Rank": 91
         },
         {
             "Category": 2016,
-            "Overall Rank": 99,
             "Economic Participation": 81,
             "Educational Attainment": 99,
             "Health and Survival": 144,
-            "Political Empowerment": 74
+            "Political Empowerment": 74,
+            "Overall Rank": 99,
         },
         {
             "Category": 2017,
-            "Overall Rank": 100,
             "Economic Participation": 86,
             "Educational Attainment": 102,
             "Health and Survival": 144,
-            "Political Empowerment": 77
+            "Political Empowerment": 77,
+            "Overall Rank": 100
         },
         {
             "Category": 2018,
-            "Overall Rank": 103,
             "Economic Participation": 86,
             "Educational Attainment": 102,
             "Health and Survival": 144,
-            "Political Empowerment":77
+            "Political Empowerment":77,
+            "Overall Rank": 103
         },
         {
             "Category": 2019,
-            "Overall Rank": 106,
             "Economic Participation": 91,
             "Educational Attainment": 100,
             "Health and Survival": 153,
-            "Political Empowerment": 95
+            "Political Empowerment": 95,
+            "Overall Rank": 106
         },
         {
             "Category": 2020,
-            "Overall Rank": 107,
             "Economic Participation": 69,
             "Educational Attainment": 103,
             "Health and Survival": 156,
-            "Political Empowerment": 118
+            "Political Empowerment": 118,
+            "Overall Rank": 107
         }
     ];
 
-    chart.data.forEach(function (o) {
 
-        o.Category = "" + o.Category;
-        o["Project Development"] = o["Project Development"] || undefined;
-
-    });
-    chart.legend = new am4charts.Legend();
-    // Set input format for the dates
-    chart.dateFormatter.inputDateFormat = "yyyy";
 
     // Create axes
     var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
     // Create series
-    ["Overall Rank",
-        "Economic Participation",
+    [ "Economic Participation",
         "Educational Attainment",
         "Health and Survival",
-        "Political Empowerment"].forEach(function (elem) {
+        "Political Empowerment",
+        "Overall Rank"].forEach(function (elem) {
 
             // Create series
             var series = chart.series.push(new am4charts.LineSeries());
@@ -196,6 +188,10 @@ am4core.ready(function () {
     // Create a horizontal scrollbar with previe and place it underneath the date axis
     chart.scrollbarX = new am4core.Scrollbar();
     chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+    chart.legend = new am4charts.Legend();
+    // Set input format for the dates
+    chart.dateFormatter.inputDateFormat = "yyyy";
 
     chart.events.on("ready", function () {
         dateAxis.zoom({ start: 0, end: 1 });
